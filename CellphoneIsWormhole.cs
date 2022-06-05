6f7b14611a42b8cba1c294a0938aa41ab25ac395
Sheepish Shepherd <cameronscottmcdougal@gmail.com>
2022-06-04 23:23:23 -0400

    Updated for June alpha version

--- CellphoneIsWormhole.cs
+++ CellphoneIsWormhole.cs
@@ -47,7 +47,7 @@ namespace CellphoneIsWormhole
 			if (item.type == ItemID.CellPhone) {
 				TooltipLine line = new TooltipLine(Mod, "CiW_WormholeTooltip1", Lang.GetTooltip(ItemID.WormholePotion).GetLine(0).ToString());
 				TooltipLine line2 = new TooltipLine(Mod, "CiW_WormholeTooltip2", Lang.GetTooltip(ItemID.WormholePotion).GetLine(1).ToString());
-				int index = tooltips.FindIndex(tt => tt.mod == "Terraria" && (tt.Name == "Expert" || tt.Name.EndsWith("Price")));
+				int index = tooltips.FindIndex(tt => tt.Mod == "Terraria" && (tt.Name == "Expert" || tt.Name.EndsWith("Price")));
 				if (index != -1) {
 					tooltips.Insert(index, line);
 					tooltips.Insert(index + 1, line2);
