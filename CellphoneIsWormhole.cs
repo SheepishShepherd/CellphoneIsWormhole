5255b1e97083729baff9798b01b141189fa16ce9
Sheepish Shepherd <cameronscottmcdougal@gmail.com>
2024-07-28 15:48:41 -0400

    Now functions with modded cell phone upgrades

--- CellphoneIsWormhole.cs
+++ CellphoneIsWormhole.cs
@@ -17,19 +17,10 @@ namespace CellphoneIsWormhole
 			On_Player.TakeUnityPotion += Player_TakeUnityPotion;
 		}
 
-		public static int[] IsConsideredUnityPotion = new int[] {
-			ItemID.CellPhone,
-			ItemID.Shellphone,
-			ItemID.ShellphoneDummy,
-			ItemID.ShellphoneHell,
-			ItemID.ShellphoneOcean,
-			ItemID.ShellphoneSpawn,
-		};
-
 		// Normally, this method would only check to see if a player has a wormhole potion
 		// Now the method will be also return true if the player has a cellphone
 		private static bool Player_HasUnityPotion(On_Player.orig_HasUnityPotion orig, Player player) {
-			foreach (int item in IsConsideredUnityPotion) {
+			foreach (int item in CellphoneSystem.IsConsideredUnityPotion) {
 				if (player.HasItemInInventoryOrOpenVoidBag(item))
 					return true;
 			}
@@ -39,7 +30,7 @@ namespace CellphoneIsWormhole
 		// Normally, this method would consume 1 wormhole potion from a player's inventory to teleport
 		// Now the method will prevent consuming wormhole potions if the player has cellphone to teleport with
 		private static void Player_TakeUnityPotion(On_Player.orig_TakeUnityPotion orig, Player player) {
-			foreach (int item in IsConsideredUnityPotion) {
+			foreach (int item in CellphoneSystem.IsConsideredUnityPotion) {
 				if (player.HasItemInInventoryOrOpenVoidBag(item))
 					return;
 			}
@@ -47,11 +38,41 @@ namespace CellphoneIsWormhole
 		}
 	}
 
+	class CellphoneSystem : ModSystem {
+		public override void PostAddRecipes() {
+			List<int> items = new List<int>();
+			foreach (int itemType in IsConsideredUnityPotion) {
+				var addItemResults = Main.recipe
+				.Take(Recipe.numRecipes)
+				.Where(r => r.HasIngredient(itemType) || r.AcceptedByItemGroups(itemType, ItemID.ShellphoneDummy));
+
+				foreach (Recipe recipe in addItemResults) {
+					if (!items.Contains(recipe.createItem.type))
+						items.Add(recipe.createItem.type);
+				}
+			}
+
+			foreach (int type in items) {
+				if (!IsConsideredUnityPotion.Contains(type))
+					IsConsideredUnityPotion.Add(type);
+			}
+		}
+
+		internal readonly static List<int> IsConsideredUnityPotion = new List<int> {
+			ItemID.CellPhone,
+			ItemID.Shellphone,
+			ItemID.ShellphoneDummy,
+			ItemID.ShellphoneHell,
+			ItemID.ShellphoneOcean,
+			ItemID.ShellphoneSpawn
+		};
+	}
+
 	class CellphoneTweak : GlobalItem
 	{
 		// Add the wormhole potion's tooltips to the cellphone as they now function identically
 		public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
-			if (CellphoneIsWormhole.IsConsideredUnityPotion.Contains(item.type)) {
+			if (CellphoneSystem.IsConsideredUnityPotion.Contains(item.type)) {
 				TooltipLine[] modToolTip = new TooltipLine[] {
 					new TooltipLine(Mod, "WormholePotion_Tooltip1", Lang.GetTooltip(ItemID.WormholePotion).GetLine(0)),
 					new TooltipLine(Mod, "WormholePotion_Tooltip2", Lang.GetTooltip(ItemID.WormholePotion).GetLine(1)),
